1dceb42eb974aa74b2123a5a0f6cbc744a344260
Murilo <muriloolegini@outlook.com>
2022-09-13 14:43:46 -0300

    first commit

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModuloAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class UsuarioController : ControllerBase
+{
+    [HttpGet("ObterDataHoraAtual")]
+    public IActionResult ObterDataHora()
+    {
+        var obj = new
+        {
+            Data = DateTime.Now.ToLongDateString(),
+            Hora = DateTime.Now.ToLongTimeString()
+        };
+
+        return Ok(obj);
+    }
+
+    [HttpGet("Apresentar/{nome}")]
+    public IActionResult Apresentar(string nome)
+    {
+        var menssagem = $"Olá {nome}, seja bem-vindo!";
+        return Ok(new { menssagem });
+    }
+}
